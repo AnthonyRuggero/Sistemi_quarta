c50e19f75e89c5dcdcde08f729ee8af1abf8c34c
Anthony Ruggero <90908033+AnthonyRuggero@users.noreply.github.com>
2022-02-15 12:14:11 +0100

    Add files via upload

---
+++ EstrazioneRandom.py
@@ -0,0 +1,7 @@
+import random 
+
+nomi = {1:"Bosticardo", 2:"Castellano", 3:"Cavaglieri", 4:"Coppola", 5:"Dalmasso", 6:"De Carlini", 7:"Degiovanni", 8:"Di Nicola", 9:"Dutto", 10:"Faggio",11:"Galfrè", 12:"Galieti", 13:"Giordano", 14:"Migliore", 15:"Molineri", 16:"Olivero", 17:"Pellegrino", 18:"Piumatto", 19:"Rebuffo", 20:"Ristorto", 21:"Ruggero", 22:"Simondi", 23:"Sparla", 24:"Sulkuqi"}
+
+sorteggio = random.randint(1,24)
+
+print(nomi[sorteggio])
